65aeb1a183dd9f6f69cfa0eef63520bae9746ee5
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2023-11-07 17:02:43 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Probot } from "probot";
+
+export = (app: Probot) => {
+  app.on("issues.labeled", async (context) => {
+    const issueLabelNames = context.payload.issue.labels?.map((label) => label.name)
+    if (issueLabelNames && issueLabelNames.includes("analysis-requested")) {
+      const parts = context.payload.issue.title.split('/')
+      const repositoryNameWithOwner = `${parts[0]}/${parts[1]}`
+      const language = parts[2]
+      context.octokit.rest.repos.createDispatchEvent({
+        owner: "pre-quantum-research",
+        repo: "pqc-data",
+        event_type: "analysis-requested",
+        client_payload: {
+          repositoryNameWithOwner: repositoryNameWithOwner,
+          language: language
+        }
+      })
+    }
+
+  });
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
